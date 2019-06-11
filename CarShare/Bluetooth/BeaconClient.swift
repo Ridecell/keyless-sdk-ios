4115e41dbbea32543491865670964d1636aa35cd
Matt Snow <matt.snow@bsmtechnologies.com>
2019-06-11 13:34:42 -0400

    beacon stuff

--- CarShare/Bluetooth/BeaconClient.swift
+++ CarShare/Bluetooth/BeaconClient.swift
@@ -8,76 +8,89 @@
 
 import CoreBluetooth
 import CoreLocation
+import RxSwift
 
 class BeaconClient: NSObject {
 
+    struct PeripheralContext {
+        let region: CLBeaconRegion
+        let peripheral: CBPeripheralManager
+        let advertisingData: [String: Any]
+        let serviceId: String
+        let characteristicId: String
+    }
+
     private enum State {
         case idle
-        case initializing(region: CLBeaconRegion, data: [String: Any])
-        case advertising(region: CLBeaconRegion)
+        case initializing(context: PeripheralContext, observer: (SingleEvent<CBPeripheralManager>) -> Void)
+        case advertising(peripheral: CBPeripheralManager)
     }
 
     private let peripheralQueue = DispatchQueue(label: "BeaconClient_PeripheralQueue")
     private let mainQueue = DispatchQueue.main
-    private lazy var peripheralManager = CBPeripheralManager(delegate: self, queue: self.peripheralQueue)
     private var state = State.idle
 
-    func startAdvertising(_ identifier: String) {
-        let uuid = UUID(uuidString: "0CEA4D3C-8193-4A26-887E-0FB5CA95F14E")!
-        log.info(uuid.uuidString)
-        let region = CLBeaconRegion(proximityUUID: uuid, major: 100, minor: 1, identifier: identifier)
+    func startAdvertising(in region: CLBeaconRegion, localName: String, serviceId: String, characteristicId: String) -> Single<CBPeripheralManager> {
+        return Single.create { observer in
 
-        var data = region.peripheralData(withMeasuredPower: nil) as! [String: Any]
-        log.info(region)
-//        let data = [
-//         data[CBAdvertisementDataManufacturerDataKey] = NSData(bytes: [UInt8(0xaf)], length: 1)
-        data[CBAdvertisementDataLocalNameKey] = "vishads-folly-returns"
-        data[CBAdvertisementDataServiceUUIDsKey] = CBUUID(string: "1809")
-//        ]
+            let peripheral = CBPeripheralManager(delegate: self, queue: self.peripheralQueue)
 
-        state = .initializing(region: region, data: data)
-        peripheralManagerDidUpdateState(peripheralManager)
+            var data = region.peripheralData(withMeasuredPower: nil) as! [String: Any]
+            data[CBAdvertisementDataLocalNameKey] = localName
+            data[CBAdvertisementDataServiceUUIDsKey] = CBUUID(string: serviceId)
+
+            self.state = .initializing(
+                context: PeripheralContext(
+                    region: region,
+                    peripheral: peripheral,
+                    advertisingData: data,
+                    serviceId: serviceId,
+                    characteristicId: characteristicId),
+                observer: observer)
+            self.peripheralManagerDidUpdateState(peripheral)
+            return Disposables.create()
+        }
     }
 
     func stopAdvertising() {
+        guard case let .advertising(peripheral) = state else {
+            return
+        }
         state = .idle
-        peripheralManager.stopAdvertising()
+        peripheral.stopAdvertising()
     }
 }
 
 extension BeaconClient: CBPeripheralManagerDelegate {
     func peripheralManagerDidUpdateState(_ peripheral: CBPeripheralManager) {
-        guard case let .initializing(region, data) = state, case .poweredOn = peripheralManager.state else {
+        guard case let .initializing(context, _) = state, case .poweredOn = peripheral.state else {
             return
         }
-        state = .advertising(region: region)
-        let service = CBMutableService(type: CBUUID(string: "1809"), primary: true)
+        let service = CBMutableService(type: CBUUID(string: context.serviceId), primary: true)
 
         service.characteristics = [
             CBMutableCharacteristic(
-                type: CBUUID(string: "2A1C"),
-                properties: [.read],
+                type: CBUUID(string: context.characteristicId),
+                properties: [.read, .write],
                 value: nil,
-                permissions: [.readable])
+                permissions: [.readable, .writeable])
         ]
-        peripheralManager.add(service)
-        log.info(data)
-
-        log.verbose("start advertising")
-//        let uuid = UUID(uuidString: "103a878d-3852-4a82-b087-2cd3756c1e06")
-
+        peripheral.add(service)
     }
 
     func peripheralManager(_ peripheral: CBPeripheralManager, didAdd service: CBService, error: Error?) {
-        if let error = error {
-            log.error(error)
-        } else {
-            log.info("added service")
-            peripheralManager.startAdvertising([
-                CBAdvertisementDataLocalNameKey: "Thermo-Supreme",
-                CBAdvertisementDataServiceUUIDsKey: [service.uuid]
-            ])
+        guard case let .initializing(context, _) = state else {
+            return
         }
+        peripheral.startAdvertising(context.advertisingData)
+    }
+
+    func peripheralManagerDidStartAdvertising(_ peripheral: CBPeripheralManager, error: Error?) {
+        guard case let .initializing(_, observer) = state else {
+            return
+        }
+        state = .advertising(peripheral: peripheral)
+        observer(.success(peripheral))
     }
 
     func peripheralManager(_ peripheral: CBPeripheralManager, didOpen channel: CBL2CAPChannel?, error: Error?) {
--- CarShare/Pages/LaunchViewController.swift
+++ CarShare/Pages/LaunchViewController.swift
@@ -6,10 +6,16 @@
 //  Copyright © 2019 BSM Technologies Inc. All rights reserved.
 //
 
+import CoreLocation
 import RxSwift
 import UIKit
 
 class LaunchViewController: UIViewController {
+
+    private enum BLeIdentifier {
+        static let service = "cbc01049-b414-473c-a0a3-d6841485e49a"
+        static let characteristic = "36eefdae-3a30-40b7-acaa-b8eb497cd1ef"
+    }
 
     static func initialize(grootWorker: GrootWorker) -> LaunchViewController {
         let viewController = LaunchViewController()
@@ -18,6 +24,7 @@ class LaunchViewController: UIViewController {
     }
 
     private var grootWorker: GrootWorker!
+    private let beaconClient = BeaconClient()
     private let disposeBag = DisposeBag()
 
     override func viewDidLoad() {
@@ -55,6 +62,21 @@ class LaunchViewController: UIViewController {
     }
 
     @objc func didTapPeripheral() {
-        log.info("did tap peripheral")
+        let uuid = UUID()
+        let region = CLBeaconRegion(
+            proximityUUID: uuid,
+            identifier: uuid.uuidString)
+
+        beaconClient.startAdvertising(
+            in: region,
+            localName: "car-share-beacon-ios",
+            serviceId: BLeIdentifier.service,
+            characteristicId: BLeIdentifier.characteristic)
+            .subscribe { event in
+                if case let .success(peripheral) = event {
+                    log.info("Advertising started! \(peripheral)")
+                }
+            }
+            .disposed(by: disposeBag)
     }
 }
