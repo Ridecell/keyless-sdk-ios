29e5742d1e04acc1d81a6215bb0bbcaeeeb15e55
Marc Maguire <marc.maguire@bsmtechnologies.com>
2019-06-24 13:23:02 -0400

    il-1303 created car share framework inside of car share project

---
+++ CarShareFramework/CarShareFramework/CarShareFramework.h
@@ -0,0 +1,19 @@
+//
+//  CarShareFramework.h
+//  CarShareFramework
+//
+//  Created by Marc Maguire on 2019-06-24.
+//  Copyright © 2019 Marc Maguire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CarShareFramework.
+FOUNDATION_EXPORT double CarShareFrameworkVersionNumber;
+
+//! Project version string for CarShareFramework.
+FOUNDATION_EXPORT const unsigned char CarShareFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CarShareFramework/PublicHeader.h>
+
+
