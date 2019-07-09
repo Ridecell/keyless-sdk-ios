ba1006dcc596b321f21f400c912c1805778170fe
Marc Maguire <marc.maguire@bsmtechnologies.com>
2019-07-09 10:36:55 -0400

    Added key signing and verifying classes to test challenge response sequence

---
+++ ChallengeSigner.swift
@@ -0,0 +1,8 @@
+//
+//  ChallengeSigner.swift
+//  CarShare
+//
+//  Created by Marc Maguire on 2019-07-08.
+//
+
+import Foundation
---
+++ ChallengeVerifier.swift
@@ -0,0 +1,8 @@
+//
+//  ChallengeVerifier.swift
+//  CarShare
+//
+//  Created by Marc Maguire on 2019-07-08.
+//
+
+import Foundation
